941d97c6f36454e1d3700936535866a199dae001
Thyago Medeiros de Oliveira <thyago.m.oliveira@bradesco.com.br>
2025-07-22 00:47:10 +0000

    1 Salario Correção valor bonus Gerente

--- Heranca/salario/Gerente.java
+++ Heranca/salario/Gerente.java
@@ -4,7 +4,9 @@ public class Gerente extends Empregado {
 
     public double calcularBonus(Departamento departamento) {
         if (departamento.alcancouMeta()) {
-            return super.getSalarioFixo() * 0.20;
+            double bonusPart1 = super.getSalarioFixo() * 0.20;
+            double bonusPart2 = (departamento.getValorAtingidoMeta() - departamento.getValorMeta()) * 0.01;
+            return bonusPart1 + bonusPart2;
         }
         return 0;
     }
