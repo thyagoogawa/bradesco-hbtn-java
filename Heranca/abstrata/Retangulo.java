10dd31d44b53584620b1645278eab7143ef288e2
Thyago Medeiros de Oliveira <thyago.m.oliveira@bradesco.com.br>
2025-07-23 19:10:32 +0000

    Herança 6 Classe Abstrata

---
+++ Heranca/abstrata/Retangulo.java
@@ -0,0 +1,36 @@
+
+public class Retangulo extends FormaGeometrica {
+    protected double largura;
+    protected double altura;
+
+    public double area() {
+        return largura * altura;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public double getLargura() {
+        return largura;
+    }
+
+    public void setAltura(double altura) {
+        if (altura < 0) {
+            throw new IllegalArgumentException("Altura deve ser maior ou igual a 0");
+        }
+        this.altura = altura;
+    }
+
+    public void setLargura(double largura) {
+        if (largura < 0) {
+            throw new IllegalArgumentException("Largura deve ser maior ou igual a 0");
+        }
+        this.largura = largura;
+    }
+
+    public String toString() {
+        return String.format("[Retangulo] %.2f / %.2f", largura, altura);
+    }
+
+}
