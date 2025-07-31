08b9d5cb59e422cc93f3cb9e36d5e68860f13f79
Thyago Medeiros de Oliveira <thyago.m.oliveira@bradesco.com.br>
2025-07-31 23:19:02 +0000

    Correção throws construtor LivroEdicaoOuro

--- 0x03/livraria/LivroEdicaoOuro.java
+++ 0x03/livraria/LivroEdicaoOuro.java
@@ -1,7 +1,9 @@
+import exceptions.AutorInvalidoException;
+import exceptions.LivroInvalidoException;
 
 public class LivroEdicaoOuro extends Livro {
 
-    public LivroEdicaoOuro(String titulo, String autor, double preco) {
+    public LivroEdicaoOuro(String titulo, String autor, double preco) throws LivroInvalidoException, AutorInvalidoException {
         super(titulo, autor, preco);
     }
 
