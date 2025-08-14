9600790e57948aa90f9fa86fce7c53620a8b7d24
Thyago Medeiros de Oliveira <thyago.m.oliveira@bradesco.com.br>
2025-08-14 00:57:32 +0000

    Arquivos - 0. Escrevendo arquivos

---
+++ arquivos/0/FileWritingExercise.java
@@ -0,0 +1,37 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class FileWritingExercise {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+
+        // Solicita o nome do arquivo
+        System.out.print("Digite o nome do arquivo (com extensão .txt): ");
+        String fileName = scanner.nextLine();
+
+
+        // implemente o codigo aqui
+        try (FileWriter fileWriter = new FileWriter(fileName)) {
+        
+            System.out.println("Digite novas linhas. Para fechar e salvar, digite 'sair' e tecle Enter :");
+            String linha = "";
+
+            while( !linha.contains("sair")) {
+                linha = scanner.nextLine();
+                fileWriter.write(linha + "\n");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        scanner.close();
+
+        System.out.println("Arquivo salvo com sucesso!");
+
+    }
+}
