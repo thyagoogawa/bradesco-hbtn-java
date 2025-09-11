735bf733284c4bd79f19e6aeea4da7e6d831ef78
Thyago Medeiros de Oliveira <thyago.m.oliveira@bradesco.com.br>
2025-09-11 00:15:32 +0000

    Generics - Commit inicial

---
+++ Generics_Exercises/armazem/Armazem.java
@@ -0,0 +1,15 @@
+public class Armazem <T> implements Armazenavel {
+
+    @Override
+    public void adicionarAoInventario(String nome, Object valor) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'adicionarAoInventario'");
+    }
+
+    @Override
+    public Object obterDoInventario(String nome) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'obterDoInventario'");
+    }
+    
+}
