5985f2a4734dcb1767b476d62cad24a3735fd37a
Thyago Medeiros de Oliveira <thyago.m.oliveira@bradesco.com.br>
2025-09-11 14:45:14 +0000

    Generics 1. Armazéns específicos

---
+++ Generics_Exercises/armazem_especifico/Armazem.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class Armazem<T> implements Armazenavel<T> {
+
+    Map<String, T> items = new HashMap<>();
+
+    @Override
+    public void adicionarAoInventario(String nome, T valor) {
+        items.put(nome, valor);
+    }
+
+    @Override
+    public T obterDoInventario(String nome) {
+        return items.get(nome);
+    }
+    
+}
---
+++ Generics_Exercises/armazem_especifico/Comida.java
@@ -0,0 +1,41 @@
+public class Comida {
+
+    private String nome;
+    private double calorias;
+    private double preco;
+
+    public Comida(String nome, double calorias, double preco) {
+        this.nome = nome;
+        this.calorias = calorias;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getCalorias() {
+        return this.calorias;
+    }
+
+    public void setCalorias(double calorias) {
+        this.calorias = calorias;
+    }
+
+    public double getPreco() {
+        return this.preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[" + this.nome + "] %,6f R$ %,6f", this.calorias, this.preco);
+    }
+}
