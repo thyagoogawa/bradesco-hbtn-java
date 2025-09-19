130f313033398eaf8f1aba96b49a78401bfde9b2
Thyago Medeiros de Oliveira <thyago.m.oliveira@bradesco.com.br>
2025-09-19 14:43:20 +0000

    Streams, Collectors e Optional 2. Relatório Produtos por preco minimo

---
+++ Streams_Collectors_Optional/preco_minimo/Consulta.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Consulta {
+    
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
+
+        List<Produto> produtosFiltrados = pedido.getProdutos().stream()
+                .filter(p -> p.getCategoriaProduto().equals(CategoriaProduto.LIVRO))
+                .collect(Collectors.toList());
+
+        return produtosFiltrados;    
+    }
+
+    // Versao 1:
+    public static Produto obterProdutoMaiorPreco(List<Produto> produtos) {
+        Produto produto = produtos.stream()
+                .sorted(Comparator.comparing(Produto::getPreco).reversed())
+                .findFirst()
+                .get();
+
+        return produto;
+    }
+
+    // Versao 2:
+    public static Produto obterProdutoMaiorPrecoV2(List<Produto> produtos) {
+        Produto produto = produtos.stream()
+                .max(Comparator.comparing(p -> p.getPreco()))
+                .get();
+
+        return produto;
+    }
+
+    public static List<Produto> obterProdutosPorPrecoMinimo(List<Produto> produtos, double precoMinimo) {
+        List<Produto> produtosFiltrados = new ArrayList<>();
+        
+        produtosFiltrados = produtos.stream()
+                .filter(p -> p.getPreco() >= precoMinimo)
+                .collect(Collectors.toList());
+        
+        return produtosFiltrados;
+    }
+
+}
