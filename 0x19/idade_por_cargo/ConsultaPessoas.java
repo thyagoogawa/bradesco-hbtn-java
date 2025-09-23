ad2a634f0475861d76f28a84dda0e173cc94f428
Thyago Medeiros de Oliveira <thyago.m.oliveira@bradesco.com.br>
2025-09-23 19:18:19 +0000

    Ajuste

--- 0x19/idade_por_cargo/ConsultaPessoas.java
+++ 0x19/idade_por_cargo/ConsultaPessoas.java
@@ -15,14 +15,6 @@ public class ConsultaPessoas {
 
     }
 
-    /*
-     *  inclua o método estático para listar a Idades das pessoas agrupadas por cargo:
-        obterIdadePorCargo: recebe uma lista de Pessoa e retorna um Map<String, List<Integer>> 
-        que contém o nome do cargo como chave do Map 
-        e a lista de idades de pessoas daquele cargo como valor do Map.
-        DICA: Utilizar Collectors.mapping
-     * 
-     */
     public static Map<String, List<Integer>> obterIdadePorCargo(List<Pessoa> pessoas) {
         
         return pessoas.stream().collect(Collectors.groupingBy(
