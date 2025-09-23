ebc9e9afdbe978c46ae6f1d9780326a7fbca6c30
Thyago Medeiros de Oliveira <thyago.m.oliveira@bradesco.com.br>
2025-09-23 19:40:06 +0000

    Ajuste

--- 0x19/idade_por_cargo/ConsultaPessoas.java
+++ 0x19/idade_por_cargo/ConsultaPessoas.java
@@ -4,17 +4,6 @@ import java.util.stream.Collectors;
 
 public class ConsultaPessoas {
 
-    public static Map<String, List<Pessoa>> obterPessoasPorCargo(List<Pessoa> pessoas) {
-
-        return pessoas.stream().collect(Collectors.groupingBy(
-            Pessoa::getCargo,
-            Collectors.mapping(
-                pessoa -> pessoa,
-                Collectors.toList()
-            )));
-
-    }
-
     public static Map<String, List<Integer>> obterIdadePorCargo(List<Pessoa> pessoas) {
         
         return pessoas.stream().collect(Collectors.groupingBy(
