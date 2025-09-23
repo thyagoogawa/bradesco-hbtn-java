ea26bd5ca1b2325ead7d5e6e3e8d9b4c0fb8e3e0
Thyago Medeiros de Oliveira <thyago.m.oliveira@bradesco.com.br>
2025-09-23 15:36:20 +0000

    Collectors 0. Pessoas agrupadas por cargo

---
+++ 0x19/pessoa_por_cargo/ConsultaPessoas.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+
+    public static Map<String, List<Pessoa>> obterPessoasPorCargo(List<Pessoa> pessoas) {
+
+        return pessoas.stream().collect(Collectors.groupingBy(
+            Pessoa::getCargo,
+            Collectors.mapping(
+                pessoa -> pessoa,
+                Collectors.toList()
+            )));
+
+    }
+}
