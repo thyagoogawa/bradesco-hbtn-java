f2685ae834ac59cd3afbfea090a75fd600eac535
Thyago Medeiros de Oliveira <thyago.m.oliveira@bradesco.com.br>
2025-09-26 15:02:35 +0000

    Streams, Collectors e Optional - Exercícios de fixação 2. Contagem de pessoas por idade e cargo

---
+++ Streams_Collector_Optional_Exercicios/quantidade_pessoas_por_cargo_idade/ConsultaPessoas.java
@@ -0,0 +1,55 @@
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toList;
+
+import java.util.Comparator;
+
+public class ConsultaPessoas {
+
+    /*
+     * O TreeSet no Collector nao permite cargos repetidos na colecao, mostrando somente o
+     * primeiro cargo que ele encontra no stream.
+     */
+    public static TreeMap<String, List<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> pessoas) {
+        return pessoas.stream()
+            .sorted(Comparator.naturalOrder())
+            .collect(groupingBy(
+                Pessoa::getCargo, // Function
+                () -> new TreeMap<>(Comparator.reverseOrder()), // Supplier 
+                toList() // Collector  
+            )
+        );
+    }
+
+    public static Map<String, Long> obterContagemPessoasPorCargo(List<Pessoa> pessoas) {
+        return pessoas.stream()
+            .collect(groupingBy(
+                Pessoa::getCargo, // Function
+                counting() // Collector
+            )
+        );
+    }
+
+    /*
+     * obterContagemPessoasPorCargoEIdade: recebe uma lista de Pessoa que retorna 
+     * um Map<String, Map<Integer, Long>> que contém o nome do cargo como chave do 
+     * primeiro Map e um Map que possua a idade e quantidade de pessoas daquela idade 
+     * e daquele cargo.
+     */
+    public static Map<String, Map<Integer, Long>> obterContagemPessoasPorCargoEIdade(List<Pessoa> pessoas) {
+        return pessoas.stream()
+            .collect(groupingBy(
+                Pessoa::getCargo, // Function
+                groupingBy( // Collector
+                    Pessoa::getIdade, // Function
+                    counting() // Collector
+                )
+            )
+        );            
+    }
+
+}
