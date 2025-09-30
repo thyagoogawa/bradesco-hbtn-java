eeee627aef144d2c6e20c001f424403649c1a160
Thyago <thyago.ogawa@gmail.com>
2025-09-30 12:42:49 +0000

    Threads 0. Sincronização com Threads

---
+++ threads/0/Principal.java
@@ -0,0 +1,16 @@
+public class Principal {
+
+    public static void main(String[] args) {
+        Contador contador = new Contador();
+
+        ThreadContador thread1 = new ThreadContador(contador);
+        ThreadContador thread2 = new ThreadContador(contador);
+
+        thread1.start();
+        thread2.start();
+
+        System.out.printf("Valor final do contador: %d\n", contador.getContagem());
+
+    }
+    
+}
